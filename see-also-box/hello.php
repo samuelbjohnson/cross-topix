c8960439064c753f9c31d6c68edd8043b5b8aa2e
Timothy Lebo <timrdf@gmail.com>
2011-04-28 23:36:05 -0400

    URL!

--- see-also-box/hello.php
+++ see-also-box/hello.php
@@ -1,33 +1,31 @@
 <?php
-$wiki_page = $_GET['page'];
+$wiki_page = 'http://imslp.org/wiki/Stabat_Mater,_Op.58_(Dvo%C5%99%C3%A1k,_Anton%C3%ADn)'; //$_GET['page'];
 $ENDPOINT = 'http://leo.tw.rpi.edu:81/endpoint.php'; 
 
-class crossTopix{
+class crossTopix {
 
-/* Utility functions */
-static function bind_variable($template, $variable, $binding) {
-   return str_replace($variable,'<'.$binding.'>',$template);
-}
+   /* Utility functions */
+   static function bind_variable($template, $variable, $binding) {
+      return str_replace($variable,'<'.$binding.'>',$template);
+   }
 
-static function prepare_query($query, $endpoint) {
-   $params           = array();
-   $params["query"]  = $query;
-   $params["output"] = 'sparql';
-   $query= $endpoint . '?' . http_build_query($params,'','&') ;
-   return $query;
-}
+   static function prepare_query($query, $endpoint) {
+      $params           = array();
+      $params["query"]  = $query;
+      $params["output"] = 'json';
+      $query= $endpoint . '?' . http_build_query($params,'','&') ;
+      return $query;
+   }
 
-static function request_query($query, $endpoint) {
-   return json_decode(file_get_contents(prepare_query($query, $endpoint)), true);
-}
+   static function request_query($query, $endpoint) {
+      return json_decode(file_get_contents(crossTopix::prepare_query($query, $endpoint)), true);
+   }
 
-static funtion getSuggestions($wiki_page, $ENDPOINT) {
+static function getSuggestions($wiki_page, $ENDPOINT) {
  $query = <<<______________________________
 PREFIX dcterms: <http://purl.org/dc/terms/>
 prefix xt:  	<http://purl.org/twc/vocab/cross-topix#>
-
 SELECT ?other ?title ?sim ?user ?accepted
-
 WHERE {
   GRAPH <http://leo.tw.rpi.edu/source/orange/dataset/crowd-verifications/version/2011-Apr-25> {
 	?vote a xt:ComparisonReview;
@@ -47,37 +45,38 @@ WHERE {
 } ORDER BY ?sim ?accepted ?user ?other ?title LIMIT 10
 ______________________________;
 
-   $query = bind_variable($query,'?:page',$wiki_page);
+   $query = crossTopix::bind_variable($query,'?:page',$wiki_page);
 
-   $result = request_query($query, $ENDPOINT);
-   
+   $result = crossTopix::request_query($query, $ENDPOINT);
+  
    if( isset($result['results']['bindings']) ) {
       foreach($result['results']['bindings'] as $binding){
-		echo '<p> <a href= ' . $binding['title']['value'] . '>' . $binding['other']['value'] . '</a> </p>';
-		
-		$val = $binding['sim']['value'];
-		echo  '<p> Machine value = ' . $val . '</p>';
-		
-		echo '<center> <p>';
-		if ($val < 0.3) {
-			echo '<img border="0" src="/full-star.png" width="30" height="30" />';
-		} else {
-			echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
-		} if ($val < 0.65) {
-			echo '<img border="0" src="/full-star.png" width="30" height="30" />';
-		} else {
-			echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
-		} if ($val < 0.8) {
-			echo '<img border="0" src="/full-star.png" width="30" height="30" />';
-		} else {
-			echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
-		}
-		echo '</p> </center>';
+         echo '<p> <a href="' . $binding['other']['value'] . '">' . $binding['title']['value'] . '</a> </p>';
+         
+         $val = $binding['sim']['value'];
+         echo  '<p> Machine value = ' . $val . '</p>';
+         
+         echo '<center> <p>';
+         if ($val < 0.3) {
+            echo '<img border="0" src="/full-star.png" width="30" height="30" />';
+         } else {
+            echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
+         } if ($val < 0.65) {
+            echo '<img border="0" src="/full-star.png" width="30" height="30" />';
+         } else {
+            echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
+         } if ($val < 0.8) {
+            echo '<img border="0" src="/full-star.png" width="30" height="30" />';
+         } else {
+            echo '<img border="0" src="/empty-star.png" width="30" height="30" />';
+         }
+         echo '</p> </center>';
       }
    }
-}
-} // end of class
+   return;
+} // end of getSuggestions
 
+} // end of class
 echo '<html>';
 echo '<body>';
 
@@ -85,5 +84,4 @@ crossTopix::getSuggestions($wiki_page, $ENDPOINT);
 
 echo '</body>';
 echo '</html>';
-
 ?> 
